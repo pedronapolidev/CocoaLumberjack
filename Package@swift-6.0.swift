b5dcb67f82f1cd41d6f82077fb96a23a816c36e7
pedronapolidev <pedro.napoli@corteva.com>
2025-12-12 02:14:40 -0300

    Change CocoaLumberjack library type to dynamic in swift 6.0 >

--- Package@swift-6.0.swift
+++ Package@swift-6.0.swift
@@ -22,6 +22,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "CocoaLumberjack",
+            type: .dynamic,
             targets: ["CocoaLumberjack"]),
         .library(
             name: "CocoaLumberjackSwift",
